2fe67b3d4d52530e4c041f22b1518ec7056acc63
Paolo M <77577216+menegop@users.noreply.github.com>
2022-05-22 22:46:06 +0200

    Fixed some bug in js

--- 4inarow/script.js
+++ 4inarow/script.js
@@ -1,9 +1,11 @@
 var gameField = new Array();
 var board = document.getElementById("game-table");
+var message = document.getElementById("message");
 var currentCol;
 var currentRow;
 var currentPlayer;
 var id = 1;
+var loading = 0
 
 class MobileNet {
     constructor() { }
@@ -23,7 +25,9 @@ class MobileNet {
 
 var nn_move
 
-newgame();
+mobileNet = new MobileNet()
+mobileNet.loadMobilenet().then(result => newgame())
+
 
 function get_state(){
     //Get the board state in javascript, make some processing for campatibility and convert to tensor
@@ -51,34 +55,32 @@ function get_state(){
 
 
 function newgame(){
+  board.innerHTML = "";
+  id = 1;
   prepareField();
-  mobileNet = new MobileNet()
-  mobileNet.loadMobilenet().then(result => placeDisc(Math.floor(Math.random()*2)+1))
-
+  placeDisc(Math.floor(Math.random()*2)+1)
 }
 
 function checkForVictory(row,col){
-  if(getAdj(row,col,0,1)+getAdj(row,col,0,-1) > 2){
-    return true;
-  } else {
-    if(getAdj(row,col,1,0) > 2){
+  if(getAdj(row,col,0,1)+getAdj(row,col,0,-1) > 2) {
       return true;
-    } else {
-      if(getAdj(row,col,-1,1)+getAdj(row,col,1,-1) > 2){
-        return true;
-      } else {
-        if(getAdj(row,col,1,1)+getAdj(row,col,-1,-1) > 2){
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
   }
+  if(getAdj(row,col,1,0) > 2) {
+      return true;
+  }
+  if(getAdj(row,col,-1,1)+getAdj(row,col,1,-1) > 2) {
+      return true;
+  }
+  if(getAdj(row,col,1,1)+getAdj(row,col,-1,-1) > 2) {
+      return true;
+  }
+  return false;
+
 }
 
 function getAdj(row,col,row_inc,col_inc){
-  if(cellVal(row,col) == cellVal(row+row_inc,col+col_inc)){
+  // A recursive function which count consecutive token of the same player in a given direction
+  if((cellVal(row,col) > 0) && (cellVal(row,col) == cellVal(row+row_inc,col+col_inc))){
     return 1+getAdj(row+row_inc,col+col_inc,row_inc,col_inc);
   } else {
     return 0;
@@ -86,6 +88,7 @@ function getAdj(row,col,row_inc,col_inc){
 }
 
 function cellVal(row,col){
+  //  return gameField if well defined, otherwise -1 (if calling outside the board)
   if(gameField[row] == undefined || gameField[row][col] == undefined){
     return -1;
   } else {
@@ -114,9 +117,9 @@ function possibleColumns(){
 }
 
 function think(){
-
-  // Convert to list for compatibility
-  nn_move = [mobileNet.predict()[0]]
+  // the AI choose the move using the model
+  message.innerHTML = "AI is thinking ..."
+  nn_move = mobileNet.predict()[0]
   console.log(nn_move)
   return nn_move;
 }
@@ -131,36 +134,43 @@ function Disc(player){
     board.innerHTML += '<div id="d'+this.id+'" class="disc '+this.color+'"></div>';
     if(currentPlayer==2){
       //computer move
-      var possibleMoves = think();
-      var cpuMove = Math.floor( Math.random() * possibleMoves.length);
-      currentCol = possibleMoves[cpuMove];
-      document.getElementById('d'+this.id).style.left = (14+60*currentCol)+"px";
+      currentCol = think();
+      document.getElementById('d'+this.id).style.left = (8+62*currentCol)+"px";
       dropDisc(this.id,currentPlayer);
+    }
+    else{
+        currentCol = 0
+        loading = 1
+        message.innerHTML = "It's your turn! - Click here to reset"
     }
   }
   
   var $this = this;
   document.onmousemove = function(evt){
     if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
+    currentCol = Math.floor((evt.clientX - board.offsetLeft)/62);
     if(currentCol<0){currentCol=0;}
     if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
+    if (loading == 1){
+        loading = 2
+    }
+    document.getElementById('d'+$this.id).style.left = (7+62*currentCol)+"px";
     document.getElementById('d'+$this.id).style.top = "-55px";
     }
   }
   document.onload = function(evt){
     if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
+    currentCol = Math.floor((evt.clientX - board.offsetLeft)/62);
     if(currentCol<0){currentCol=0;}
     if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
+    document.getElementById('d'+$this.id).style.left = (7+62*currentCol)+"px";
     document.getElementById('d'+$this.id).style.top = "-55px";
     }
   }
   
   document.onclick = function(evt){
-    if(currentPlayer == 1){
+    if(currentPlayer == 1 && loading == 2){
+
       if(possibleColumns().indexOf(currentCol) != -1){
         dropDisc($this.id,$this.player);
       }
@@ -170,21 +180,46 @@ function Disc(player){
 
 function dropDisc(cid,player){
   currentRow = firstFreeRow(currentCol,player);
-  moveit(cid,(14+currentRow*60));
-  currentPlayer = player;
-  checkForMoveVictory();
+  console.log("current", player, currentRow, currentCol)
+  moveit(cid,(9+currentRow*62));
+
+  console.log(id)
+
+  if (! checkForMoveVictory()){
+    currentPlayer = 3 - player;
+    var disc = new Disc(currentPlayer);
+    disc.addToScene()
+    }
+  else{
+      $this = ""
+  }
+
+
 }
 
+message.onmouseup=function (){
+    loading = true
+
+    newgame()
+}
+
+
 function checkForMoveVictory(){
-  if(!checkForVictory(currentRow,currentCol)){
-    placeDisc(3-currentPlayer);
-  } else {
-    var ww = currentPlayer == 2 ? 'Computer' : 'Player';
-    placeDisc(3-currentPlayer);
-    alert(ww+" win!");
-    board.innerHTML = "";
-    newgame();
+  //  Check if a player has win with last move
+
+  if(checkForVictory(currentRow,currentCol)) {
+    var ww = currentPlayer == 2 ? 'AI' : 'Player';
+    message.innerHTML = ww+" win! Click here to play again"
+    return true
+    // alert(ww+" win!");
+    // board.innerHTML = "";
+    // newgame();
   }
+  if (id == 44){
+      message.innerHTML = "Draw! Click here to play again"
+      return true
+  }
+  return false
 }
 
 function placeDisc(player){
@@ -205,4 +240,8 @@ function prepareField(){
 
 function moveit(who,where){
     document.getElementById('d'+who).style.top = where+'px';
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
